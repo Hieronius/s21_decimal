683c81ea4ecfd6cf53d3409e746f33930c9d6355
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-15 04:22:40 +0300

    Create a new project

---
+++ s21_Decimal/s21_Decimal/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  s21_Decimal
+//
+//  Created by Арсентий Халимовский on 15.03.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
