c2ebc7bf3579874885e839425a3c0ad296c3142f
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-19 16:41:51 +0300

    add function's prototypes

--- s21_Decimal/s21_Decimal/s21_decimal.h
+++ s21_Decimal/s21_Decimal/s21_decimal.h
@@ -2,6 +2,51 @@
 #define MINUS 0x80000000 // 10000000 00000000 00000000 00000000
 #define SC 0x00ff0000 // 00000000 11111111 00000000 00000000 SCALE
 
+// MARK: Арифметические операторы
+// Сложение +
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result)
+// Вычитание -
+int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result)
+// Умножение *
+int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result)
+// Деление /
+int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result)
+
+// MARK: Операторы сравнения
+// Меньше <
+int s21_is_less(s21_decimal, s21_decimal)
+//Меньше или равно <=
+int s21_is_less_or_equal(s21_decimal, s21_decimal)
+// Больше >
+int s21_is_greater(s21_decimal, s21_decimal)
+// Больше или равно >=
+int s21_is_greater_or_equal(s21_decimal, s21_decimal)
+// Равно ==
+int s21_is_equal(s21_decimal, s21_decimal)
+// Не равно !=
+int s21_is_not_equal(s21_decimal, s21_decimal)
+
+
+// MARK: Преобразовали
+// Из int
+int s21_from_int_to_decimal(int src, s21_decimal *dst)
+// Из float
+int s21_from_float_to_decimal(float src, s21_decimal *dst)
+// В int
+int s21_from_decimal_to_int(s21_decimal src, int *dst)
+// В float
+int s21_from_decimal_to_float(s21_decimal src, float *dst)
+ 
+// MARK: Другие функции
+// Округляет указанное Decimal число до ближайшего целого числа в сторону отрицательной бесконечности.
+int s21_floor(s21_decimal value, s21_decimal *result)
+// Округляет Decimal до ближайшего целого числа.
+int s21_round(s21_decimal value, s21_decimal *result)
+// Возвращает целые цифры указанного Decimal числа; любые дробные цифры отбрасываются, включая конечные нули.
+int s21_truncate(s21_decimal value, s21_decimal *result)
+//Возвращает результат умножения указанного Decimal на -1.
+int s21_negate(s21_decimal value, s21_decimal *result)
+
 /*
  
  Максимальные и минимальные значения INT32 и s21_decimal:
