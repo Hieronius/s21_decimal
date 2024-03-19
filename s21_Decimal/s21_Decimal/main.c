6b0c83045bc621d99e65a5612b1ca7de3350dbf8
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-19 03:41:58 +0300

    Remove unused code

--- s21_Decimal/s21_Decimal/main.c
+++ s21_Decimal/s21_Decimal/main.c
@@ -1,14 +1,8 @@
-//
-//  main.c
-//  s21_Decimal
-//
-//  Created by Арсентий Халимовский on 15.03.2024.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     return 0;
+
 }
