46f2a2e5aae4ee55886c0440644029c45ce64a84
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-31 04:29:12 +0300

    Add missing brackets

--- s21_Decimal/s21_Decimal/other.c
+++ s21_Decimal/s21_Decimal/other.c
@@ -19,10 +19,11 @@ int s21_round(s21_decimal value, s21_decimal *result) {
         if (s21_getSign(value)) {
             s21_sub(value, ((s21_decimal){{5, 0, 0, 65536}}), &value);
         } else {
-          s21_add(value, ((s21_decimal){{5, 0, 0, 65536}}), &value);
-        s21_truncate(value, result);
-      }
-      return 0;
+            s21_add(value, ((s21_decimal){{5, 0, 0, 65536}}), &value);
+            s21_truncate(value, result);
+        }
+    }
+    return 0;
 }
 
 // Возвращает целые цифры указанного Decimal числа; любые дробные цифры отбрасываются, включая конечные нули.
