06e2e8a030db96fc3785c0e7314e1aef2d917c82
Arsenty Halimovsky <ghost12@yandex.ru>
2024-04-01 03:54:19 +0300

    FIx comments

--- s21_Decimal/s21_Decimal/other.c
+++ s21_Decimal/s21_Decimal/other.c
@@ -33,7 +33,7 @@ int s21_truncate(s21_decimal value, s21_decimal *result) {
       return 0;
 }
 
-//Возвращает результат умножения указанного Decimal на -1.
+// Возвращает результат умножения указанного Decimal на -1.
 int s21_negate(s21_decimal value, s21_decimal *result) {
     s21_copy(s21_reset(result),
              s21_getSign(value) ? *s21_setSign(&value, 0) : *s21_setSign(&value, 1));
--- s21_Decimal/s21_Decimal/s21_decimal.h
+++ s21_Decimal/s21_Decimal/s21_decimal.h
@@ -57,7 +57,7 @@ int s21_floor(s21_decimal value, s21_decimal *result);
 int s21_round(s21_decimal value, s21_decimal *result);
 // Возвращает целые цифры указанного Decimal числа; любые дробные цифры отбрасываются, включая конечные нули.
 int s21_truncate(s21_decimal value, s21_decimal *result);
-//Возвращает результат умножения указанного Decimal на -1.
+// Возвращает результат умножения указанного Decimal на -1.
 int s21_negate(s21_decimal value, s21_decimal *result);
 
 // MARK: Дополнительные функции
