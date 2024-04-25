ecb0cef6c3a922656e3a6d43edad7f95975ba063
Arsenty Halimovsky <ghost12@yandex.ru>
2024-04-25 03:44:19 +0300

    Add comment

--- s21_Decimal/s21_Decimal/comparison.c
+++ s21_Decimal/s21_Decimal/comparison.c
@@ -47,6 +47,7 @@ int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
   return res;
 }
 
+// Больше или равно
 int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
   return s21_is_greater(value_1, value_2) || s21_is_equal(value_1, value_2);
 }
