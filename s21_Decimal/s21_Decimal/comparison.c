bcab2fb17365ffe4d275dc3e9f57cfbdec62f661
Arsenty Halimovsky <ghost12@yandex.ru>
2024-04-27 03:43:51 +0300

    Add comment

--- s21_Decimal/s21_Decimal/comparison.c
+++ s21_Decimal/s21_Decimal/comparison.c
@@ -52,11 +52,12 @@ int s21_is_greater_or_equal(s21_decimal value_1, s21_decimal value_2) {
   return s21_is_greater(value_1, value_2) || s21_is_equal(value_1, value_2);
 }
 
-// Меньше или равно
+// Меньше
 int s21_is_less(s21_decimal value_1, s21_decimal value_2) {
   return s21_is_greater(value_2, value_1);
 }
 
+// Меньше или равно
 int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2) {
   return s21_is_less(value_1, value_2) || s21_is_equal(value_1, value_2);
 }
