3935f67901405031db02d73f7514cf3af6807da0
Arsenty Halimovsky <ghost12@yandex.ru>
2024-04-28 03:36:53 +0300

    Add comment

--- s21_Decimal/s21_Decimal/comparison.c
+++ s21_Decimal/s21_Decimal/comparison.c
@@ -62,6 +62,7 @@ int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2) {
   return s21_is_less(value_1, value_2) || s21_is_equal(value_1, value_2);
 }
 
+// Не равно
 int s21_is_not_equal(s21_decimal value_1, s21_decimal value_2) {
   return !s21_is_equal(value_1, value_2);
 }
